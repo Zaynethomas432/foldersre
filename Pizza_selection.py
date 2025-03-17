2077dedc0a7d6c41373b5a4f0855648e44b0cee1
Zaynethomas432 <zaynethomas@riccarton.school.nz>
2025-03-18 10:06:54 +1300

    fixed index error

--- Pizza_selection.py
+++ Pizza_selection.py
@@ -1,6 +1,6 @@
 """This code will ask the user for what pizzas they want and how many they need and if 0 will exclude the pizza from final answer"""
 #variables for storage
-listpiz = []
+listpizza = []
 stop = 0
 
 
@@ -11,63 +11,68 @@ while True:
 
         cheese = int(input("How many cheese pizzas do we want? "))
         if cheese > stop:
-            listpiz.append(cheese)
+            listpizza.append(cheese)
             break
         if cheese == stop:
+            listpizza.append(cheese)
             break
         if cheese < stop:
             print("Please enter a valid amount")
     except ValueError:
         print("Please enter a valid amount")
-while True:
+while True:#loop until right option for chicken
     try:
 
         chicken = int(input("How many chicken pizzas do we want? "))
         if chicken > stop:
-            listpiz.append(chicken)
+            listpizza.append(chicken)
             break
         if chicken == stop:
+            listpizza.append(cheese)
             break
         if chicken < stop:
             print("Please enter a valid amount")
     except ValueError:
         print("Please enter a valid amount")
-while True:
+while True:#loop until right option for pepperoni
     try:
 
         pepperoni = int(input("How many pepperoni pizzas do we want? "))
         if pepperoni > stop:
-            listpiz.append(pepperoni)
+            listpizza.append(pepperoni)
             break
         if pepperoni == stop:
+            listpizza.append(cheese)
             break
         if pepperoni < stop:
             print("Please enter a valid amount")
     except ValueError:
         print("Please enter a valid amount")
-while True:
+while True:#loop until right option for veggie
     try:
 
         veggie = int(input("How many veggie pizzas do we want? "))
         if veggie > stop:
-            listpiz.append(veggie)
+            listpizza.append(veggie)
             break
         if veggie == stop:
+            listpizza.append(cheese)
             break
         if veggie < stop:
             print("Please enter a valid amount")
     except ValueError:
         print("Please enter a valid amount")
-    #print the wanted order
 
-if cheese != stop:
-    print(f"Cheese: {listpiz[0]}")
-if chicken != stop:
-    print(f"Chicken: {listpiz[1]}")
-if pepperoni != stop:
-    print(f"Pepperoni: {listpiz[2]}")
-if veggie != stop:
-    print(f"Veggie: {listpiz[3]}")
+#print the wanted order
+
+if listpizza[0] != stop: #!= stop makes it so it wont print if the value of the variable is 0 
+    print(f"Cheese: {listpizza[0]}")
+if listpizza[1] != stop:
+    print(f"Chicken: {listpizza[1]}")
+if listpizza[2] != stop:
+    print(f"Pepperoni: {listpizza[2]}")
+if listpizza[3] != stop:
+    print(f"Veggie: {listpizza[3]}")
         
         
 
