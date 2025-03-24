43ee117795b1ffaedc8673cf9892186c09ad889f
Zaynethomas432 <zaynethomas@riccarton.school.nz>
2025-03-25 09:37:33 +1300

    just debugging

--- NCEA1_code_assesment(standard_1.1).py
+++ NCEA1_code_assesment(standard_1.1).py
@@ -1,8 +1,10 @@
 """this code will ask the user for a speed, then display to them the unsafe speeds they have inputting."""
 #define variables for the code
-stop = 'terminate'
+
 good_descent = 10
 bad_speed = 0
+stop = 'done'
+
 #define lists of all speeds 
 speedlist = []
 #ask the user to input
@@ -16,12 +18,20 @@ while speed != stop:# making the code run until 'terminate' is entered
         print('Error, invalid input.')
     speed = input('Input descent speed in m/s: ')
 
+
 #find the amount of shuttles too fast. 
+
 for item in speedlist:
     if item > good_descent:
         bad_speed += 1 
 
 
+
+#print the number of too fast shuttles
+if bad_speed != 1:
+    print(f"There were {bad_speed} space shuttles faster than the safe speed.")
+else:
+    print(f"There was 1 space shuttle faster than the safe speed.")
     
 
 
