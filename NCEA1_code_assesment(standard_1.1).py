ff0294393490016f4dfcd4cbfdbb8cdd07a94784
Zaynethomas432 <zaynethomas@riccarton.school.nz>
2025-03-25 09:03:01 +1300

    made a while loop to make the code run until terminate is entered

--- NCEA1_code_assesment(standard_1.1).py
+++ NCEA1_code_assesment(standard_1.1).py
@@ -1,13 +1,22 @@
 """this code will ask the user for a speed, then display to them the unsafe speeds they have inputting."""
 #define variables for the code
 stop = 'terminate'
-descent = 10
-badspeed = 0
+good_descent = 10
+bad_speed = 0
 #define lists of all speeds 
 speedlist = []
 #ask the user to input
 speed = input('Input descent speed in m/s: ')
 #loop to keep on asking for input until code is terminated
+while speed != stop:# making the code run until 'terminate' is entered 
+    #check for valid speed
+    try:
+        speedlist.append(float(speed))#append to list
+    except:
+        print('Error, invalid input.')
+    speed = input('Input descent speed in m/s: ')
+
 
     
+
 
