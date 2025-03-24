09b1f4cbc5dcd445d1bfd672bb2874f7b1744702
Zaynethomas432 <zaynethomas@riccarton.school.nz>
2025-03-25 08:51:39 +1300

    made input for speed

--- NCEA1_code_assesment(standard_1.1).py
+++ NCEA1_code_assesment(standard_1.1).py
@@ -1,5 +1,13 @@
 """this code will ask the user for a speed, then display to them the unsafe speeds they have inputting."""
+#define variables for the code
 stop = 'terminate'
 descent = 10
 badspeed = 0
+#define lists of all speeds 
 speedlist = []
+#ask the user to input
+speed = input('Input descent speed in m/s: ')
+#loop to keep on asking for input until code is terminated
+
+    
+
